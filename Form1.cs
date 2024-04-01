c736251a62b31adc4650ad41a2c48dc380137a41
s1120371 <s1120371@o365st.pu.edu.tw>
2024-04-01 11:17:36 +0800

    teacher

--- Form1.cs
+++ Form1.cs
@@ -46,37 +46,37 @@ namespace OXGame
         private void checkWinner()
         {
             // 
-            if (button1.Text == button2.Text && button2.Text == button3.Text && button1.Text != "")
+            if (button1.Text == button2.Text && button2.Text == button3.Text && button1.Text != "" )
             {
                 MessageBox.Show(button1.Text, "Winner is...");
             }
-            else if (button4.Text == button5.Text && button5.Text == button6.Text && button4.Text != "")
+            else if (button4.Text == button5.Text && button5.Text == button6.Text && button4.Text != "" )
             {
                 MessageBox.Show(button4.Text, "Winner is...");
             }
-            else if (button7.Text == button8.Text && button8.Text == button9.Text && button7.Text != "")
+            else if (button7.Text == button8.Text && button8.Text == button9.Text && button7.Text != "" )
             {
                 MessageBox.Show(button7.Text, "Winner is...");
             }
-            else if (button1.Text == button4.Text && button4.Text == button7.Text && button1.Text != "")
+            else if (button1.Text == button4.Text && button4.Text == button7.Text && button1.Text != "" )
             {
                 MessageBox.Show(button1.Text, "Winner is...");
             }
-            else if (button2.Text == button5.Text && button5.Text == button8.Text && button2.Text != "")
+            else if (button2.Text == button5.Text && button5.Text == button8.Text && button2.Text != "" )
             {
                 MessageBox.Show(button2.Text, "Winner is...");
             }
-            else if (button3.Text == button6.Text && button6.Text == button9.Text && button3.Text != "")
+            else if (button3.Text == button6.Text && button6.Text == button9.Text && button3.Text != "" )
             {
                 MessageBox.Show(button3.Text, "Winner is...");
             }
-            else if (button1.Text == button5.Text && button5.Text == button9.Text && button1.Text != "")
+            else if (button1.Text == button5.Text && button5.Text == button9.Text && button1.Text != "" )
             {
                 MessageBox.Show(button1.Text, "Winner is...");
             }
-            else if (button3.Text == button5.Text && button5.Text == button7.Text && button3.Text != "")
+            else if (button3.Text == button5.Text && button5.Text == button7.Text && button3.Text != "" )
             {
-                MessageBox.Show(button3.Text, "Winner is...");
+                MessageBox.Show(button3.Text, "Winner is..." );
             }
         }
     }
